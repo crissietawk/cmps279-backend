8021451292e67f29b1798b86e314f73d7eff2f80
Reine Zeineddine <rhz06@mail.aub.edu>
2025-10-19 22:42:45 +0300

    fixing deploy error

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ recording, notes, analysis, and notifications
 """
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import api_router
+from app.router import api_router
 from app.db import get_connection
 import uvicorn
 
